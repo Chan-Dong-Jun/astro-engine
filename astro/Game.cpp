27e156e1391671a000986a98623c85298ffdbe4e
Dong Jun <121388798+Chan-Dong-Jun@users.noreply.github.com>
2025-01-18 23:56:27 +0800

    set up pe

---
+++ astro/Game.cpp
@@ -0,0 +1,6 @@
+//
+// Created by dj8ch on 18/1/2025.
+//
+
+#include "Game.h"
+
---
+++ astro/Game.h
@@ -0,0 +1,37 @@
+//
+// Created by dj8ch on 18/1/2025.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#include "SDL.h"
+#include <iostream>
+
+
+class Game {
+
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() { return isRunning;};
+
+private:
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+
+};
+
+
+
+#endif //GAME_H
